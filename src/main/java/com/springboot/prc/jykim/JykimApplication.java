75958dba95f4541054c5a4e6115d3539370e050d
jykim-rust <alexj99@naver.com>
2022-12-24 15:18:10 +0900

    first commit

---
+++ src/main/java/com/springboot/prc/jykim/JykimApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.prc.jykim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JykimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JykimApplication.class, args);
+	}
+
+}
